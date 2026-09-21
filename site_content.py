d62f1627890c218034826382a7dbdada403b3f8d
SYM <sym@SYMdeMacBook-Air.local>
2026-09-21 12:15:47 +0800

    Update contact email

--- site_content.py
+++ site_content.py
@@ -140,7 +140,7 @@ SITE = SiteData(
             )
         ],
         links=[
-            LinkItem(label="Email", url="mailto:CST2209162@xmu.edu.my"),
+            LinkItem(label="Email", url="mailto:sujiayi2003@gmail.com"),
             LinkItem(label="Github", url="https://github.com/ShuYuMo2003"),
             LinkItem(label="X", url="https://x.com/JiaYiSu8"),
             LinkItem(label="OI Blog", url="oi-blog/index.html"),
